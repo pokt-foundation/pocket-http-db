f77e9631575de68b66915f6e0aec12856183d760
Pablo Ocampo <pablojocampogo@gmail.com>
2022-10-19 11:29:55 -0400

    fix: add notify event to init db

--- tests/init-db.sql
+++ tests/init-db.sql
@@ -175,6 +175,39 @@ VALUES
     ('TEST_PLAN_10K', 10000),
     ('TEST_PLAN_90K', 90000);
 
+CREATE OR REPLACE FUNCTION notify_event() RETURNS TRIGGER AS $$
+
+    DECLARE 
+        data json;
+        notification json;
+    
+    BEGIN
+    
+        -- Convert the old or new row to JSON, based on the kind of action.
+        -- Action = DELETE?             -> OLD row
+        -- Action = INSERT or UPDATE?   -> NEW row
+        IF (TG_OP = 'DELETE') THEN
+            data = row_to_json(OLD);
+        ELSE
+            data = row_to_json(NEW);
+        END IF;
+        
+        -- Contruct the notification as a JSON string.
+        notification = json_build_object(
+                          'table',TG_TABLE_NAME,
+                          'action', TG_OP,
+                          'data', data);
+        
+                        
+        -- Execute pg_notify(channel, notification)
+        PERFORM pg_notify('events',notification::text);
+        
+        -- Result is ignored since this is an AFTER trigger
+        RETURN NULL; 
+    END;
+    
+$$ LANGUAGE plpgsql;
+
 CREATE TRIGGER application_notify_event
 AFTER INSERT OR UPDATE ON applications
     FOR EACH ROW EXECUTE PROCEDURE notify_event();
